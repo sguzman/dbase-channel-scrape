6eb5036262e667d520a440e2fcb6334093c842f7
Salvador Guzman <guzmansalv@gmail.com>
2018-10-14 04:25:47 -0700

    perfect

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 
 #include "curl.hxx"
 
+
 inline static unsigned long max_pages() noexcept {
   const auto text{Curl::get("https://dbase.tube/chart/channels/subscribers/all")};
 
@@ -42,7 +43,7 @@ inline static std::vector<std::string> page(const unsigned long idx) noexcept {
   }
 
   CSelection tags{doc.find("a[href^=\"/c/\"]")};
-  std::vector<std::string> chans{tags.nodeNum()};
+  std::vector<std::string> chans{};
   {
     for (auto i{0u}; i < tags.nodeNum(); ++i) {
       const auto href_raw{tags.nodeAt(i).attribute("href")};
@@ -76,11 +77,26 @@ int main(int argc, char* argv[]) noexcept {
   }
 
   for (const auto& v : vec) {
+    pqxx::connection C{"dbname=youtube user=postgres host=192.168.1.63 port=30339"};
+    pqxx::work W{C};
+
     const auto chans{page(v)};
     for (const auto& c : chans) {
       std::cout << c << std::endl;
+
+      std::string insert{"INSERT INTO youtube.entities.channels (serial) VALUES ('"};
+      insert += c;
+      insert += "') ON CONFLICT (serial) DO NOTHING;";
+
+      W.exec(insert);
     }
+
+    W.commit();
+    C.disconnect();
+    C.deactivate();
   }
+
+
 
   return 0;
 }
