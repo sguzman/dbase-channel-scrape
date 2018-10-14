f26b701e8aed81f47e1dd86b87ac01c8e1fbbec7
Salvador Guzman <guzmansalv@gmail.com>
2018-10-13 23:42:52 -0700

    remove modules

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM alpine
+MAINTAINER Salvador Guzman <guzmansalv@gmail.com>
+
+RUN apk add --no-cache build-base cmake
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <iostream>
+#include <gumbo.h>
+#include <pqxx/pqxx>
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+}
