cb4074268dc8fa91a0cc14370c7e0bfb25df729e
Salvador Guzman <guzmansalv@gmail.com>
2018-10-19 06:13:44 -0700

    while true

--- src/main.cpp
+++ src/main.cpp
@@ -62,41 +62,39 @@ inline static std::vector<std::string> page(const unsigned long idx) noexcept {
   return chans;
 }
 
-int main(int argc, char* argv[]) noexcept {
-  const auto pages{max_pages()};
+void main(int argc, char* argv[]) noexcept {
+  while (true) {
+    const auto pages{max_pages()};
 
-  std::vector<unsigned long> vec{pages};
-  {
-    for (auto i{1u}; i <= pages; ++i) {
-      vec.push_back(i);
+    std::vector<unsigned long> vec{pages};
+    {
+      for (auto i{1u}; i <= pages; ++i) {
+        vec.push_back(i);
+      }
+
+      std::random_device rd;
+      std::mt19937 g{rd()};
+      std::shuffle(vec.begin(), vec.end(), g);
     }
 
-    std::random_device rd;
-    std::mt19937 g{rd()};
-    std::shuffle(vec.begin(), vec.end(), g);
-  }
+    for (const auto& v : vec) {
+      pqxx::connection C{"dbname=youtube user=postgres host=postgres-service.youtube.svc.cluster.local port=5432"};
+      pqxx::work W{C};
 
-  for (const auto& v : vec) {
-    pqxx::connection C{"dbname=youtube user=postgres host=192.168.1.63 port=30339"};
-    pqxx::work W{C};
+      const auto chans{page(v)};
+      for (const auto& c : chans) {
+        std::cout << c << std::endl;
 
-    const auto chans{page(v)};
-    for (const auto& c : chans) {
-      std::cout << c << std::endl;
+        std::string insert{"INSERT INTO youtube.entities.channels (serial) VALUES ('"};
+        insert += c;
+        insert += "') ON CONFLICT (serial) DO NOTHING;";
 
-      std::string insert{"INSERT INTO youtube.entities.channels (serial) VALUES ('"};
-      insert += c;
-      insert += "') ON CONFLICT (serial) DO NOTHING;";
+        W.exec(insert);
+      }
 
-      W.exec(insert);
+      W.commit();
+      C.disconnect();
+      C.deactivate();
     }
-
-    W.commit();
-    C.disconnect();
-    C.deactivate();
   }
-
-
-
-  return 0;
 }
